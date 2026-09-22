55ea8e3eee625573731576f20e8254724bb9aac0
TheRealPchol <therealpchol7@gmail.com>
2026-09-22 20:47:37 +0300

    Начал делать вывод результата

--- reference.py
+++ reference.py
@@ -1,10 +1,11 @@
+import datetime
 import os
 import logging
 import csv
 from models import Tag, Platform, Game
 from client import StopGameClient
 from bs4 import BeautifulSoup as bs
-
+from models import Game
 logger = logging.getLogger(__name__)
 
 CONFIG_DIR = "config"
@@ -96,25 +97,13 @@ def get_games(page: str = 1, tags: list = None, platforms: list = None) -> list[
                     #print(url)
                     info_by_soup(soup)
                     break
-def info_by_soup(soup, show: bool = False, _step: str = "", _start: str = "\n"):
-    result = {}
+def info_by_soup(soup, show: bool = False, _step: str = "", _start: str = "\n") -> models.Game:
+    result = models.Game()
     date_of_out = soup.find("div", class_="_game-info_1bso0_696").find("dl", class_="_game-info__grid_1bso0_993").find_all("dd")[0].text
-    result["date"] = date_of_out
+    result.date = datetime.strptime(date_of_out, "%d.%m.%Y")
     name = soup.find('h1', class_="_game-title_1bso0_702").text
-    result["Название"] = name
-    #print(date_of_out.prettify())
-    dts = soup.find("dl", class_="_game-info__grid_1bso0_993").find_all("dt")
-    dds = soup.find("dl", class_="_game-info__grid_1bso0_993").find_all("dd")
-    for i in range(len(dds)):
-        print()
 
 
-
-    if show:
-        print(f"{_start}{_step}Информация о {name}:")
-        print(f"{_step}    Дата выхода: {date_of_out}")
-        #print(f"{_step}    Разработчик: {author}")
-        #print(type(games_all))
 if __name__ == "__main__":
     # from client import StopGameClient
     # from logging_config import BASIC_CONFIG
